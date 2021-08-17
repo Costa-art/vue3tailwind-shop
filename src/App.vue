9a756f83279bad76c94c3259d69f35594fc1b384
Costa-art <78592321+Costa-art@users.noreply.github.com>
2021-08-17 21:32:44 +0300

    2 commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home2</router-link> |
     <router-link to="/about">About</router-link>
   </div>
   <router-view />
